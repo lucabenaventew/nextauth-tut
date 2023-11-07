93e5c79742027ed226e0f369e9dba3c02b69061b
Luca Benavente <cacachuuuluca@gmail.com>
2023-11-07 15:21:22 -0500

    last commit

--- app/felizceroochobb/page.tsx
+++ app/felizceroochobb/page.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image'
+
 export default function Greetings() {
 	return (
-		<div>
-			Feliz 08 bb 💕💕💕 te quiero mucho, espero que te guste tu regalito
-		</div>
+		<main>
+			<div>
+				Feliz 08 bb 💕💕💕 te quiero mucho, espero que te guste tu regalito
+			</div>
+			<Image
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png"
+				width={100}
+				height={100}
+				alt=""
+			/>
+		</main>
 	)
 }
